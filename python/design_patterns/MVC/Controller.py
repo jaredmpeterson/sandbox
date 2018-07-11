119b1ca472eb8052c61f673d3edfa3a366b4912f
Jared M. Peterson <developer@peteyinc.com>
2018-07-11 17:02:44 -0600

    crude add_person to Controller

--- python/design_patterns/MVC/Controller.py
+++ python/design_patterns/MVC/Controller.py
@@ -7,11 +7,20 @@ def show_all():
     return View.show_all_view(people_in_db)
 
 
+def add_person():
+    first = input('First name: ')
+    last = input('Last name: ')
+    person = Person(str(first), str(last))
+    return person
+
+
 def start():
     View.start_view()
     answer = input()
     if answer == 'y':
         return show_all()
+    elif answer == 'a':
+        return add_person()
     else:
         return View.end_view()
 
